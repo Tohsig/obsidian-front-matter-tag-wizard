fde790e706c7edd4a98fdc637b177578509f8d5f
tohsig <eric@jotdashed.com>
2022-02-06 13:37:49 -0500

    Strip quotes and hash signs from frontmatter tags

--- src/yamlFormatTags.ts
+++ src/yamlFormatTags.ts
@@ -1,17 +1,21 @@
+const reReplace = /[:\s,#"']+/g;
+const reSplit = /[^:\s,#"']+/g;
+
 function parseTags(text: string) {
 	let tagSet: Set<string>;
 
 	if (text.includes("\n")) {
 		const [_head, ...tail] = text
 			.split("\n")
-			.map((t) => t.replace("-", "").trim())
+			.map((t) => t.replace(reReplace, "").trim())
 			.filter((t) => t.length);
+
 		tagSet = new Set(tail);
 	} else {
-		const match = text.match(/[^:\s,]+/g);
+		const match = text.match(reSplit);
 		if (match !== null) {
 			const [_head, ...tail] = match;
-			tagSet = new Set(tail);
+			tagSet = new Set(tail.filter((t) => t.length));
 		} else {
 			tagSet = new Set();
 		}
